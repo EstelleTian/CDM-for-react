f07abe11d496fc9b1e0dd3221896920fdb081ffe
krorainaHub <zxy920411@163.com>
2018-07-20 11:22:51 +0800

    reducer 添加拦截用户登出

--- src/redux/reducers.jsx
+++ src/redux/reducers.jsx
@@ -5,12 +5,22 @@ import { tableDatas, tableCondition,  generateInfo, generateTime } from "../comp
 import { flowcontrolDatas, flowcontrolCondition, flowGenerateTime } from "../components/FlowcontrolModule/Redux"
 import { subTableDatas } from "../components/SubTable/Redux"
 
-const reducer = combineReducers({
+const appReducer = combineReducers({
     loginUserInfo, systemConfig, flowcontrolParams,
     tableDatas, tableCondition, generateInfo, generateTime,
     filterMatches,
     subTableDatas,
     flowcontrolDatas, flowcontrolCondition, flowGenerateTime
 });
+
+
+const reducer = (state, action) => {
+
+    if (action.type === 'userLogout') { // 拦截用户登出
+        state = undefined //并没有在这里改变状态, 只是重新分配一个局部变量state的引用,
+                          // 然后再传递给另一个函数。
+    }
+    return appReducer(state, action)
+}
 
 export default reducer;
